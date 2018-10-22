030334a1f431e1c1b1fe1fce0c59fb4d39f81e78
elmer7451 <yul.etxabe@gmail.com>
2018-10-22 13:14:49 +0200

    clase Anotacion

--- app/src/main/java/com/example/yulechave/proyectoanotaciones/Anotacion.java
+++ app/src/main/java/com/example/yulechave/proyectoanotaciones/Anotacion.java
@@ -1,4 +1,39 @@
 package com.example.yulechave.proyectoanotaciones;
 
 public class Anotacion {
+
+    private String titulo;
+    private String fecha;
+    private String anotacion;
+
+
+    public Anotacion(String titulo, String fecha, String anotacion) {
+        this.titulo = titulo;
+        this.fecha = fecha;
+        this.anotacion = anotacion;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getAnotacion() {
+        return anotacion;
+    }
+
+    public void setAnotacion(String anotacion) {
+        this.anotacion = anotacion;
+    }
 }
